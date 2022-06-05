fba55a7c95cedfc56b311505485bdc62a0c139f0
Suhan Moon <leader@trabricks.io>
2022-06-05 22:17:38 +0900

    Style Processing.

--- src/devtools/network_badge.tsx
+++ src/devtools/network_badge.tsx
@@ -1,31 +1,69 @@
-import React, { useMemo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useMemo, useRef } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
 
 import { NetworkLogItem } from './common';
 
-const NetworkBadge = ({ data }: { data: NetworkLogItem[] }) => {
-  const items = useMemo(() => {
-    return data.slice(0, 3);
-  }, [data]);
+const BACKGROUND_FOR_STATE = {
+  ok: '#339933',
+  fail: '#db2929',
+  ready: '#aaa',
+};
 
+const NetworkBadge = ({ data, show }: { data: NetworkLogItem[]; show: boolean }) => {
+  const opacity = useRef(new Animated.Value(0)).current;
+  const tk = useRef<unknown>(0);
+
+  const items = useMemo(() => data.slice(0, 3), [data]);
   const first = items?.[0];
 
+  useEffect(() => {
+    if (tk.current && typeof tk.current === 'number') {
+      clearTimeout(tk.current);
+    }
+
+    Animated.timing(opacity, {
+      toValue: data?.length > 0 ? 1 : 0,
+      duration: 150,
+      useNativeDriver: true,
+    }).start();
+
+    const f = data?.[0];
+    if (data?.length > 0 && f?.state !== 'ready') {
+      tk.current = setTimeout(() => {
+        Animated.timing(opacity, {
+          toValue: 0,
+          duration: 150,
+          useNativeDriver: true,
+        }).start();
+      }, 2000);
+    }
+  }, [`${data?.[0]?.state}-${data?.[0]?.time}`]);
+
+  if (!show) return null;
+
   return (
-    <View
-      style={[StyleSheet.absoluteFill, { backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }]}
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFill,
+        { opacity, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' },
+      ]}
     >
       <View style={{ flexDirection: 'row', marginTop: 4 }}>
-        {items.map(() => (
-          <View
-            style={{
-              width: 4,
-              height: 4,
-              borderRadius: 2,
-              backgroundColor: '#aaa',
-              marginHorizontal: 1,
-            }}
-          />
-        ))}
+        {items.map(({ state }) =>
+          !state ? (
+            undefined
+          ) : (
+            <View
+              style={{
+                backgroundColor: BACKGROUND_FOR_STATE?.[state],
+                width: 4,
+                height: 4,
+                borderRadius: 2,
+                marginHorizontal: 1,
+              }}
+            />
+          ),
+        )}
       </View>
       <View
         style={{
@@ -43,9 +81,9 @@ const NetworkBadge = ({ data }: { data: NetworkLogItem[] }) => {
         </Text>
       </View>
       <Text style={{ fontSize: 8, lineHeight: 12, height: 12 }} allowFontScaling={false}>
-        {first?.state}
+        {!first?.finish ? '-' : first?.finish - (first?.time ?? 0)}
       </Text>
-    </View>
+    </Animated.View>
   );
 };
 
